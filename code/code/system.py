8b7614be989a183857a91978167ed1d861aa3c16
Kevin <kevinwu454@gmail.com>
2023-12-12 03:15:23 +0000

    Almost there

--- code/code/system.py
+++ code/code/system.py
@@ -8,23 +8,19 @@ produce a very poor result.
 
 version: v1.0
 """
+from collections import Counter
 from typing import List
 
 import numpy as np
 from scipy import linalg
+from scipy.spatial import distance
+from scipy.ndimage import gaussian_filter
 
 N_DIMENSIONS = 10
 
-def euclidean_distance(x1, x2):
-    return np.sqrt(np.sum((x1 - x2)**2))
 
 def classify(train: np.ndarray, train_labels: np.ndarray, test: np.ndarray) -> List[str]:
     """Classify a set of feature vectors using a training set.
-
-    This dummy implementation simply returns the empty square label ('.')
-    for every input feature vector in the test data.
-
-    Note, this produces a surprisingly high score because most squares are empty.
 
     Args:
         train (np.ndarray): 2-D array storing the training feature vectors.
@@ -34,67 +30,22 @@ def classify(train: np.ndarray, train_labels: np.ndarray, test: np.ndarray) -> L
     Returns:
         list[str]: A list of one-character strings representing the labels for each square.
     """
-    '''
-    predictions = []
-    for test_vector in test:
-        distances = np.sqrt(np.sum((euclidean_distance(train[:1600], test))**2, axis=0))
-        nearest_neighbours = np.argsort(distances)[:k]
-        most_frequent_label = np.bincount(train_labels[nearest_neighbours]).argmax()
-        predictions = np.append(predictions, most_frequent_label)
-    return predictions
-    '''
-    k=10
-    
-    # Super compact implementation of nearest neighbour
-    x = np.dot(test, train.transpose())
-    modtest = np.sqrt(np.sum(test * test, axis=1))
-    modtrain = np.sqrt(np.sum(train * train, axis=1))
-    dist = x / np.outer(modtest, modtrain.transpose())
-    
-    # cosine distance
-    nearest = np.argmax(dist, axis=1) #[:k]
-    mdist = np.max(dist, axis=1)
-    
-    # Get the closest k
-    k_indices = np.argsort(dist)[:k]
-    print("k indices shape : ", k_indices.shape)
-    print("k indices : ", k_indices)
-    k_nearest_labels = train_labels[k_indices]
-    
 
-    label = train_labels[nearest]
-    print("label shape :",label.shape)
-    print("Label (nearest neighbour no k) : ", label)
-    print("k nearest labels : ", k_nearest_labels)
+    k=5
     
-    k_nearest_labels = k_nearest_labels.transpose()
-    print("k nearest labels shape : ", k_nearest_labels.shape)
-    # Attempted knn implementation
-    most_freq_labels = np.unique(k_nearest_labels)
-    # print(most_freq_labels)
-    # print(len(most_freq_labels))
-    # print(most_freq_labels[1].shape)
-    # print(most_freq_labels[0])
-    # print(most_freq_labels[1])
-    print("This is [0] shape ", most_freq_labels[0].shape)
-    return k_nearest_labels
-
-
-    # # Find the k nearest neighbors
-    # nearest = np.argpartition(dist, k, axis=0)[:k]
-
-    # # Get the labels of the k nearest neighbors
-    # nearest_labels = train_labels[nearest]
-
-    # # Use majority voting to decide the label
-    # labels = stats.mode(nearest_labels, axis=0)
-    # labels = labels.ravel()
-
-    # return labels.tolist()
+    # Euclidean distance using Scipy.
+    dist = distance.cdist(test, train, 'euclidean')
     
+    # Get the indices of the k nearest neighbours
+    k_nearest = np.argsort(dist, axis=1)[:,:k] 
+
+    k_nearest_labels = train_labels[k_nearest]
+
+    # Uses majority voting to get the most common labels.
+    majority_labels = np.array([Counter(row).most_common(1)[0][0] for row in k_nearest_labels])
     
-    # n_images = test.shape[0]
-    # return ["."] * n_images
+    return majority_labels
+
 
 
 # The functions below must all be provided in your solution. Think of them
@@ -120,75 +71,13 @@ def reduce_dimensions(data: np.ndarray, model: dict) -> np.ndarray:
         np.ndarray: The reduced feature vectors.
     """
     
-    nmax = 8  # number of pca components
-    sample_index = 5
-
-
+    eigenvectors_train = np.array(model["eigenvectors"])
     
-    # Calculate the covariance matrix
-    covariance_matrix = np.cov(data, rowvar=0)
-
-    # Calculate the eigenvalues and eigenvectors
-    N = covariance_matrix.shape[0]
-    # print(covariance_matrix.shape)
-    
-    # N = covariance_matrix.shape[0]
-    # eigenvectors = linalg.eigh(covariance_matrix, eigvals=(N - 10, N - 1))
-    v = linalg.eigh(covariance_matrix, eigvals=(N - 10, N - 1))
-    eigenvalues = v[0]
-    eigenvectors = v[1]
-    # print(np.shape(eigenvectors))
-    # print(np.shape(eigenvalues))
-    # print("before shape", np.shape(data))
-    # # Projecting the data onto the principal components axis.
-    pca_data = np.dot((data - np.mean(data)), eigenvectors)
-    # print("pca shape : ", np.shape(pca_data))
-    # reconstructed = np.dot(pcatrain_data, v.transpose()) + np.mean(train_data) #reconstructing the data back from 10 to 2500 dimensions, I don't see the need for it.
+    pca_data = np.dot(data - np.mean(data), eigenvectors_train)
     reduced_data = pca_data 
 
-    # eigenvectors = np.fliplr(eigenvectors)
-    # Step 4: Sort the eigenvectors by decreasing eigenvalues
-    # and choose the first k eigenvectors
-    # W = Vt.T[:, :model[10]]
-    
-    data_mean = np.mean(data, axis=0)
-
-    # data_list = data.tolist()
-    # for n in range(1, nmax):
-    #     npca = n * 2
-    #     reconstructed = (
-    #         np.dot(
-    #             np.dot(data - data_mean, eigenvectors[:, 0:npca]), eigenvectors[:, 0:npca].transpose()
-    #         )
-    #         + data_mean
-    #     )
-    #     data_list.append(reconstructed)
-    
-    # N=6
-    # reconstructed = (
-    #     np.dot(
-    #         np.dot(data[0, :] - data_mean, eigenvectors[:, 0 : N - 1]),
-    #         eigenvectors[:, 0 : N - 1].transpose(),
-    #     )
-    #     + data_mean
-    # )
-    # print(reconstructed.shape)
-    # print(reconstructed)
-    # print(np.ndim(reconstructed))
-    # reduced_data = data[:, 0:N_DIMENSIONS]
-    # print(len(data_list))
     return reduced_data 
     
-    # pca_data = np.dot((data - np.mean(data)), v)
-    # N = 6 # project the images from the data set into N-dimensional PCA space.
-    # nmax = 8 # number of pca components
-    # reconstructed = (
-    #     np.dot(
-    #         np.dot(data[0, :] - mean_train, v[:, 0 : N - 1]),
-    #         v[:, 0 : N - 1].transpose(),
-    #     )
-    #     + mean_train
-    # )
     # reduced_data = data[:, 0:N_DIMENSIONS]
 
 
@@ -206,11 +95,18 @@ def process_training_data(fvectors_train: np.ndarray, labels_train: np.ndarray) 
         dict: a dictionary storing the model data.
     """
 
-    # The design of this is entirely up to you.
-    # Note, if you are using an instance based approach, e.g. a nearest neighbour,
-    # then the model will need to store the dimensionally-reduced training data and labels.
     model = {}
     model["labels_train"] = labels_train.tolist()
+    
+    # Get the covariance matrix of the training data
+    covariance_matrix = np.cov(fvectors_train, rowvar=0)
+
+    # Calculate the eigenvectors
+    N = covariance_matrix.shape[0]
+    v = linalg.eigh(covariance_matrix, eigvals=(N - 10, N - 1))
+    eigenvectors = v[1]
+    
+    model["eigenvectors"] = eigenvectors.tolist()
     fvectors_train_reduced = reduce_dimensions(fvectors_train, model)
     model["fvectors_train"] = fvectors_train_reduced.tolist()
     return model
@@ -231,8 +127,8 @@ def images_to_feature_vectors(images: List[np.ndarray]) -> np.ndarray:
     n_features = h * w
     fvectors = np.empty((len(images), n_features))
     for i, image in enumerate(images):
+        image = gaussian_filter(image, sigma=1) # Apply Gaussian filter
         fvectors[i, :] = image.reshape(1, n_features)
-
     return fvectors
 
 
@@ -257,7 +153,7 @@ def classify_squares(fvectors_test: np.ndarray, model: dict) -> List[str]:
 
     # Call the classify function.
     labels = classify(fvectors_train, labels_train, fvectors_test)
-
+    
     return labels
 
 
@@ -278,5 +174,43 @@ def classify_boards(fvectors_test: np.ndarray, model: dict) -> List[str]:
     Returns:
         list[str]: A list of one-character strings representing the labels for each square.
     """
+    # Call the classify_squares function.
+    labels = classify_squares(fvectors_test, model)
 
-    return classify_squares(fvectors_test, model)
+    # Reshape the labels into an array representing the boards.
+    labels = labels.reshape(-1, 8, 8)
+    
+    for i in range(labels.shape[0]):
+        # Count the number of kings of each color
+        num_white_kings = np.count_nonzero(labels[i] == 'K')
+        num_black_kings = np.count_nonzero(labels[i] == 'k')
+        num_white_pawns = np.count_nonzero(labels[i] == 'P')
+        num_black_pawns = np.count_nonzero(labels[i] == 'p')
+        num_white_queens = np.count_nonzero(labels[i] == 'Q')
+        num_black_queens = np.count_nonzero(labels[i] == 'q')
+
+        # If a board has more than one king of either color, change one of them.
+        if num_white_kings > 1 or num_black_kings > 1:
+            print("More than two kings")
+            for j in range(labels.shape[1]): 
+                for k in range(labels.shape[2]):  
+                    if labels[i][j][k] == 'K':
+                        labels[i][j][k] = 'Q'
+                        break
+                        
+                    if labels[i][j][k] == 'k':
+                        labels[i][j][k] = 'q'
+                        break
+            
+        if num_white_pawns > 8 or num_black_pawns > 8:
+            print("More than 8 pawns")
+            
+        if num_white_queens > 9 or num_black_queens > 9:
+            print("More than 9 queens")
+            
+            
+    labels = labels.reshape(-1,)
+    
+    return labels
+
+
